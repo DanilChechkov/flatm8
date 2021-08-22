3380633f1fb8671b05d0b25bfa133b4e7a56b56c
DanilChechkov <danilchechkov@icloud.com>
2021-08-22 18:18:21 +0300

    fixing of no fixing

--- room8d/flmate/views.py
+++ room8d/flmate/views.py
@@ -202,11 +202,11 @@ def edit(request):
                         if not hUser.email in notiData.keys():
                             notiData[hUser.email] = [timezone.now().date() -td(days=1),timezone.now().date() -td(days=1)]
                         if timezone.now().date() > notiData[hUser.email][0]:
-                            #hUser.email_user("FLATMATE - мы нашли тебе соседа!", 
-                            #        'Привет! Мы нашли тебе соседа, осталось только написать ему! --> https://flatm8.ru/dialogs/\nКстати от уведомлений можно отписаться тут --> https://flatm8.ru/edit/\nЕсли что-то работает не так дай нам об этом знать - DanilChechkov@flatm8.ru', 
-                            #            'flatmate@flatm8.ru')
-                            txt = 'succes'
-                            send_mail("TEST",txt,'flatmate@flatm8.ru',['danilchechkov@icloud.com'])
+                            hUser.email_user("FLATMATE - мы нашли тебе соседа!", 
+                                    'Привет! Мы нашли тебе соседа, осталось только написать ему! --> https://flatm8.ru/dialogs/\nКстати от уведомлений можно отписаться тут --> https://flatm8.ru/edit/\nЕсли что-то работает не так дай нам об этом знать - DanilChechkov@flatm8.ru', 
+                                        'flatmate@flatm8.ru')
+                            #txt = 'succes'
+                            #send_mail("TEST",txt,'flatmate@flatm8.ru',['danilchechkov@icloud.com'])
                             notiData[hUser.email][0] = timezone.now().date()
                         with open(notifFILE, 'wb') as f:
                             pickle.dump(notiData, f)
