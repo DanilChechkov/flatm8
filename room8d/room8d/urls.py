224a480370037adaa39d2686049496a223f1c16a
DanilChechkov <danilchechkov@icloud.com>
2021-08-24 18:13:28 +0300

    fixifix

--- room8d/room8d/urls.py
+++ room8d/room8d/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -10,5 +12,9 @@ urlpatterns = [
     url(r'^$', RedirectView.as_view(url='index/', permanent=False), name='index'),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
