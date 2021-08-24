2d68c85b8ba78d6143264173edd4f947a92bcb52
DanilChechkov <danilchechkov@icloud.com>
2021-08-24 21:24:35 +0300

    admin panel

--- room8d/flmate/admin.py
+++ room8d/flmate/admin.py
@@ -4,19 +4,19 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'last_login', 'first_name') # Added last_login
+    list_display = ('username','last_login','email', 'first_name') # Added last_login
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user','active','urAge', 'rmAgeL','rmAgeU','aprUGEN','aprR8GEN','aprTELLUS','photo']
+    list_display = ['user','active','urAge','contInsta','contTeleg','contVKont', 'rmAgeL','rmAgeU','aprUGEN','aprR8GEN','aprTELLUS','photo']
                 #['user','active', 'urAge', 'rmAgeL','rmAgeU','rntLPrice','rntUPrice','rntTime','rntSubway',
                 #'abuLST','abuCOMU','abuBADIC','abuORGL',
                 #'abrTEMP','abrSOUL','abrCLEAN','abrGUEST','abrCOMMUNISM',
                 #'aprUGEN','aprR8GEN','aprURRELIGY','aprR8RELIGY','aprFRETM','aprPETS','aprTELLUS','photo']
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ['chat','author','message','is_readed']
+    list_display = ['chat','is_readed','author','message']
 class ChatAdmin(admin.ModelAdmin):
     list_display = ['get_members','cap','sub']
 
