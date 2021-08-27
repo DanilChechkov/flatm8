a84b9838f4868a90d2878be27cf897cc49714d0e
DanilChechkov <danilchechkov@icloud.com>
2021-08-27 17:59:44 +0300

    sda

--- room8d/flmate/views.py
+++ room8d/flmate/views.py
@@ -83,133 +83,135 @@ def edit(request):
             
             now = datetime.datetime.now()
             checked = []
-            me = request.user.profile.__class__.objects.filter(pk=request.user.profile.id).values().first()
-            myAge = int(me.get('urAge'))
-            myRAge = [int(me.get('rmAgeL')),int(me.get('rmAgeU'))]
-            myPrice = [int(me.get('rntLPrice')),int(me.get('rntUPrice'))]
-            myRTime = str(me.get('rntTime'))
-            mySubway = [ x for x in me.get('rntSubway')]
-            myHrono = str(me.get('abuLST'))
-            myCOMU = str(me.get('abuCOMU'))
-            myBadic = [ x for x in me.get('abuBADIC')]
-            myOrgL = int(me.get('abuORGL'))
-            myTemp = int(me.get('abrTEMP'))
-            mySouL = str(me.get('abrSOUL'))
-            myClean = int(me.get('abrCLEAN'))
-            myGuest = str(me.get('abrGUEST'))
-            mymoL = str(me.get('abrCOMMUNISM'))
-            myGen = str(me.get('aprUGEN'))
-            myRGen = str(me.get('aprR8GEN'))
-            myRel = str(me.get('aprURRELIGY'))
-            myRRel = str(me.get('aprR8RELIGY'))
-            myFrtime = [ x for x in me.get('aprFRETM')]
-            myPets = str(me.get('aprPETS'))
-            
-            for you in profileList:
-                if not you.get('active'):
-                    inactiveUser = User.objects.get(id=you.get('user_id'))
-
-                    lastlog = inactiveUser.last_login
-                    lastpos = timezone.now()- td(days=21)
-                    if lastlog:
-                        if lastlog<lastpos:
-                            for chat in Chatroom.objects.all():
-                                if inactiveUser in chat.members.all():
-                                    chat.delete()
-                            inactiveUser.email_user("FLATMATE: До встречи!", 
-                                    'Спасибо, ' + hUser.username +'!\nТы замечательный человек и мы уверены - ты нашел своего соседа! Уже 3 недели как ты не заходил на сайт.\nЕсли мы снова понадобимся тебе или твоим друзьям --> https://flatm8.ru/ \nДо новых встреч, дорогой друг!', 
-                                        'flatmate@flatm8.ru')
-                            inactiveUser.profile.delete()
-                            inactiveUser.delete()
-                    continue
-                else:
-                    UserToSwitch = User.objects.get(id=you.get('user_id'))
-                    lastlog = UserToSwitch.last_login
-                    if lastlog:
-                        lastpos = timezone.now()- td(days=14)
-                        if lastlog<=lastpos:
-                            UserToSwitch.profile.active = False
-                            UserToSwitch.profile.save()
-                            UserToSwitch.email_user("FLATMATE - твой аккаунт деактивирован!", 
-                                    'Привет, ' + hUser.username +'!\nСайт растет и число пользователей ежедневно увеличивается! Ты не заходил на сайт более двух недель и мы решили, что ты больше не ищешь соседа, поэтому деактивировали твой профиль. Если мы ошиблись - заходи на сайт и твой профиль снова активируется, иначе твой аккаунт будет безвозвратно удален через неделю! --> https://flatm8.ru/', 
-                                        'flatmate@flatm8.ru')
-                            continue    #IF EVERYTHING F*CK UP DELETE THIS LINE
-                         
-                if me == you or [me.get('user_id'),you.get('user_id')] in checked: continue
-                points = 0
-                frtme = 0
-                subinte=0
-
-                hAge = int(you.get('urAge'))
-                hRAge = [int(you.get('rmAgeL')),int(you.get('rmAgeU'))]
-                hPrice = [int(you.get('rntLPrice')),int(you.get('rntUPrice'))]
-                hRTime = str(you.get('rntTime'))
-                hSubway = [ x for x in you.get('rntSubway')]
-                hHrono = str(you.get('abuLST'))
-                hCOMU = str(you.get('abuCOMU'))
-                hBadic = [ x for x in you.get('abuBADIC')]
-                hOrgL = int(you.get('abuORGL'))
-                hTemp = int(you.get('abrTEMP'))
-                hSouL = str(you.get('abrSOUL'))
-                hClean = int(you.get('abrCLEAN'))
-                hGuest = str(you.get('abrGUEST'))
-                hmoL = str(you.get('abrCOMMUNISM'))
-                hGen = str(you.get('aprUGEN'))
-                hRGen = str(you.get('aprR8GEN'))
-                hRel = str(you.get('aprURRELIGY'))
-                hRRel = str(you.get('aprR8RELIGY'))
-                hFrtime = [ x for x in you.get('aprFRETM')]
-                hPets = str(you.get('aprPETS'))
-
-                checked.append([me.get('user_id'),you.get('user_id')])
-                checked.append([you.get('user_id'),me.get('user_id')])
-
-                #MAIN FILTERS: AGE,PRICE,RELIGY,GENDER,
-                if not hRAge[0]<=myAge<=hRAge[1] or not myRAge[0]<=hAge<=myRAge[1]: continue
-                if not (hPrice[0]<=myPrice[1]<=hPrice[1] or myPrice[0]<=hPrice[1]<=myPrice[1]):continue
-                if not ((hRRel == myRel or hRRel == 'nosing') and (myRRel == hRel or myRRel == 'nosing')): continue
-                if not ((hRGen == myGen or hRGen == 'inbetween') and (myRGen == hGen or myRGen == 'inbetween')): continue
-                if myRTime != hRTime: continue
-                points += 15
-
-                #COUNT POINTS, NOT VERY IMPORTANT POINTS
-                if myRTime == hRTime: points +=1                #TERM
-                if myHrono == hHrono: points +=1                 #Hronos
-                if myCOMU == hCOMU: points +=1                   #Extrav/Introv
-                if myOrgL-2<=hOrgL<=myOrgL+2: points +=1        #Organisation level
-                if myTemp-2<=hTemp<=myTemp+2: points +=1        #Comfort temperature
-                if mySouL == hSouL:points +=1                   #Sound level
-                if myClean-2<=hClean<=myClean+2: points +=1     #Cleaning
-                if myGuest == hGuest:points +=1                 #Guests
-                if mymoL == hmoL:points +=1                     #COMMUNISM  
-                if myPets==hPets or (myPets == 'nomater' or hPets == 'nomater'): points +=1  #PETS
-                if len(list(set(myFrtime)&set(hFrtime))) != 0:
-                    points += int((len(list(set(myFrtime)&set(hFrtime)))/10)*5) #Freetime intersections MAX 5POINTS!!!
-
-                #SPECIAL SUBWAY FIELD, EVEN IF NO MATCH IT STILL COUNTS, BUT LATER subinte WILL SAY NO/YES MATCHES SUBWAY
-                subInter = list(set(mySubway)&set(hSubway))
-                if len(subInter)==0: subinte = 1
-
-                #Bad addictions
-                badInter = list(set(myBadic)&set(hBadic))
-                if len(badInter)>0: points +=1
-                #SAVING ROOMATES
-                capa = 100*points/31
-
+            #me = request.user.profile.__class__.objects.filter(pk=request.user.profile.id).values().first()
+            for me in profileList:
+                if not me.get('active'):continue
+                myAge = int(me.get('urAge'))
+                myRAge = [int(me.get('rmAgeL')),int(me.get('rmAgeU'))]
+                myPrice = [int(me.get('rntLPrice')),int(me.get('rntUPrice'))]
+                myRTime = str(me.get('rntTime'))
+                mySubway = [ x for x in me.get('rntSubway')]
+                myHrono = str(me.get('abuLST'))
+                myCOMU = str(me.get('abuCOMU'))
+                myBadic = [ x for x in me.get('abuBADIC')]
+                myOrgL = int(me.get('abuORGL'))
+                myTemp = int(me.get('abrTEMP'))
+                mySouL = str(me.get('abrSOUL'))
+                myClean = int(me.get('abrCLEAN'))
+                myGuest = str(me.get('abrGUEST'))
+                mymoL = str(me.get('abrCOMMUNISM'))
+                myGen = str(me.get('aprUGEN'))
+                myRGen = str(me.get('aprR8GEN'))
+                myRel = str(me.get('aprURRELIGY'))
+                myRRel = str(me.get('aprR8RELIGY'))
+                myFrtime = [ x for x in me.get('aprFRETM')]
+                myPets = str(me.get('aprPETS'))
                 
-                mUser = User.objects.get(id=me.get('user_id'))
-                hUser = User.objects.get(id=you.get('user_id'))
-                chatlist = Chatroom.objects.values()
-                if checkChatr(mUser,hUser,chatlist,capa,subinte):
-                    createChatroom(mUser,hUser,capa,subinte)
-                    if hUser.profile.chatNotif:
-                        hUser.email_user("FLATMATE - мы нашли тебе соседа!", 
-                                'Привет, ' + hUser.username +'!'+
-                                '\nМы нашли тебе соседа, осталось только написать ему! --> https://flatm8.ru/dialogs/'+
-                                '\n\nКстати от уведомлений можно отписаться тут --> https://flatm8.ru/edit/'+
-                                '\nЕсли что-то работает не так дай нам об этом знать DanilChechkov@flatm8.ru', 
-                                    'flatmate@flatm8.ru')
-                    #NOTIF SYS OVER
+                for you in profileList:
+                    if not you.get('active'):
+                        inactiveUser = User.objects.get(id=you.get('user_id'))
+
+                        lastlog = inactiveUser.last_login
+                        lastpos = timezone.now()- td(days=21)
+                        if lastlog:
+                            if lastlog<lastpos:
+                                for chat in Chatroom.objects.all():
+                                    if inactiveUser in chat.members.all():
+                                        chat.delete()
+                                inactiveUser.email_user("FLATMATE: До встречи!", 
+                                        'Спасибо, ' + hUser.username +'!\nТы замечательный человек и мы уверены - ты нашел своего соседа! Уже 3 недели как ты не заходил на сайт.\nЕсли мы снова понадобимся тебе или твоим друзьям --> https://flatm8.ru/ \nДо новых встреч, дорогой друг!', 
+                                            'flatmate@flatm8.ru')
+                                inactiveUser.profile.delete()
+                                inactiveUser.delete()
+                        continue
+                    else:
+                        UserToSwitch = User.objects.get(id=you.get('user_id'))
+                        lastlog = UserToSwitch.last_login
+                        if lastlog:
+                            lastpos = timezone.now()- td(days=14)
+                            if lastlog<=lastpos:
+                                UserToSwitch.profile.active = False
+                                UserToSwitch.profile.save()
+                                UserToSwitch.email_user("FLATMATE - твой аккаунт деактивирован!", 
+                                        'Привет, ' + hUser.username +'!\nСайт растет и число пользователей ежедневно увеличивается! Ты не заходил на сайт более двух недель и мы решили, что ты больше не ищешь соседа, поэтому деактивировали твой профиль. Если мы ошиблись - заходи на сайт и твой профиль снова активируется, иначе твой аккаунт будет безвозвратно удален через неделю! --> https://flatm8.ru/', 
+                                            'flatmate@flatm8.ru')
+                                continue    #IF EVERYTHING F*CK UP DELETE THIS LINE
+                            
+                    if me == you or [me.get('user_id'),you.get('user_id')] in checked: continue
+                    points = 0
+                    frtme = 0
+                    subinte=0
+
+                    hAge = int(you.get('urAge'))
+                    hRAge = [int(you.get('rmAgeL')),int(you.get('rmAgeU'))]
+                    hPrice = [int(you.get('rntLPrice')),int(you.get('rntUPrice'))]
+                    hRTime = str(you.get('rntTime'))
+                    hSubway = [ x for x in you.get('rntSubway')]
+                    hHrono = str(you.get('abuLST'))
+                    hCOMU = str(you.get('abuCOMU'))
+                    hBadic = [ x for x in you.get('abuBADIC')]
+                    hOrgL = int(you.get('abuORGL'))
+                    hTemp = int(you.get('abrTEMP'))
+                    hSouL = str(you.get('abrSOUL'))
+                    hClean = int(you.get('abrCLEAN'))
+                    hGuest = str(you.get('abrGUEST'))
+                    hmoL = str(you.get('abrCOMMUNISM'))
+                    hGen = str(you.get('aprUGEN'))
+                    hRGen = str(you.get('aprR8GEN'))
+                    hRel = str(you.get('aprURRELIGY'))
+                    hRRel = str(you.get('aprR8RELIGY'))
+                    hFrtime = [ x for x in you.get('aprFRETM')]
+                    hPets = str(you.get('aprPETS'))
+
+                    checked.append([me.get('user_id'),you.get('user_id')])
+                    checked.append([you.get('user_id'),me.get('user_id')])
+
+                    #MAIN FILTERS: AGE,PRICE,RELIGY,GENDER,
+                    if not hRAge[0]<=myAge<=hRAge[1] or not myRAge[0]<=hAge<=myRAge[1]: continue
+                    if not (hPrice[0]<=myPrice[1]<=hPrice[1] or myPrice[0]<=hPrice[1]<=myPrice[1]):continue
+                    if not ((hRRel == myRel or hRRel == 'nosing') and (myRRel == hRel or myRRel == 'nosing')): continue
+                    if not ((hRGen == myGen or hRGen == 'inbetween') and (myRGen == hGen or myRGen == 'inbetween')): continue
+                    if myRTime != hRTime: continue
+                    points += 15
+
+                    #COUNT POINTS, NOT VERY IMPORTANT POINTS
+                    if myRTime == hRTime: points +=1                #TERM
+                    if myHrono == hHrono: points +=1                 #Hronos
+                    if myCOMU == hCOMU: points +=1                   #Extrav/Introv
+                    if myOrgL-2<=hOrgL<=myOrgL+2: points +=1        #Organisation level
+                    if myTemp-2<=hTemp<=myTemp+2: points +=1        #Comfort temperature
+                    if mySouL == hSouL:points +=1                   #Sound level
+                    if myClean-2<=hClean<=myClean+2: points +=1     #Cleaning
+                    if myGuest == hGuest:points +=1                 #Guests
+                    if mymoL == hmoL:points +=1                     #COMMUNISM  
+                    if myPets==hPets or (myPets == 'nomater' or hPets == 'nomater'): points +=1  #PETS
+                    if len(list(set(myFrtime)&set(hFrtime))) != 0:
+                        points += int((len(list(set(myFrtime)&set(hFrtime)))/10)*5) #Freetime intersections MAX 5POINTS!!!
+
+                    #SPECIAL SUBWAY FIELD, EVEN IF NO MATCH IT STILL COUNTS, BUT LATER subinte WILL SAY NO/YES MATCHES SUBWAY
+                    subInter = list(set(mySubway)&set(hSubway))
+                    if len(subInter)==0: subinte = 1
+
+                    #Bad addictions
+                    badInter = list(set(myBadic)&set(hBadic))
+                    if len(badInter)>0: points +=1
+                    #SAVING ROOMATES
+                    capa = 100*points/31
+
+                    
+                    mUser = User.objects.get(id=me.get('user_id'))
+                    hUser = User.objects.get(id=you.get('user_id'))
+                    chatlist = Chatroom.objects.values()
+                    if checkChatr(mUser,hUser,chatlist,capa,subinte):
+                        createChatroom(mUser,hUser,capa,subinte)
+                        if hUser.profile.chatNotif:
+                            hUser.email_user("FLATMATE - мы нашли тебе соседа!", 
+                                    'Привет, ' + hUser.username +'!'+
+                                    '\nМы нашли тебе соседа, осталось только написать ему! --> https://flatm8.ru/dialogs/'+
+                                    '\n\nКстати от уведомлений можно отписаться тут --> https://flatm8.ru/edit/'+
+                                    '\nЕсли что-то работает не так дай нам об этом знать DanilChechkov@flatm8.ru', 
+                                        'flatmate@flatm8.ru')
+                        #NOTIF SYS OVER
 
             checked = []
             return render(request, 'account/dialogs.html', {'user_profile': request.user, 'chats': chats,'section':'dialogs'})
