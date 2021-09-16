1646459f214c0cb70aeac4076b27df52cff65207
DanilChechkov <danilchechkov@icloud.com>
2021-09-16 17:28:05 +0300

    compatibility %%

--- room8d/flmate/views.py
+++ room8d/flmate/views.py
@@ -180,7 +180,7 @@ def edit(request):
                 if len(badInter)>0: points +=1
                 #SAVING ROOMATES
                 capa = 100*points/31
-                if capa<65: continue
+                if capa<62: continue
                 createChatroom(mUser,hUser,capa,subinte)
                 if hUser.profile.chatNotif:
                     hUser.email_user("FLATMATE - мы нашли тебе соседа!", 
